eb36b8a38cd2d54600d74dc61bedcbf721a721d0
yzc-yangmo <2403577910@qq.com>
2025-03-12 17:46:15 +0800

    add two new papers: PathRWKV,CMGN

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 const allProjects = [
   {
+    title: "CMGN: Text GNN and RWKV MLP-mixer combined with cross-feature fusion for fake news detection",
+    description: "he paper proposes a novel cross-feature fusion network, CMGN, combining Text Graph Neural Networks (GNN) and RWKV MLP-mixer for fake news detection. The RWKV MLP-mixer processes news text by replacing self-attention with MLP layers to capture deep semantic features, while Text GNN models relationships among supplementary texts (e.g., titles, locations) as graph nodes. A cross-feature fusion mechanism integrates these features dynamically. Evaluated on LIAR, FA-KES, IFND, and CHEF datasets, CMGN outperforms existing methods, demonstrating enhanced accuracy. Focal loss addresses class imbalance, and ablation studies confirm RWKV's critical role in feature extraction. The model advances fake news detection by synergizing graph-based relational modeling and efficient text-sequence processing via RWKV.",
+    date: "2025-03-12",
+    tags: "Sequence",
+    img: "images/papers-images/img-20250312-1.png",
+    link: "https://www.sciencedirect.com/science/article/abs/pii/S0925231225004837"
+  },  {
+    title: "PathRWKV: Enabling Whole Slide Prediction with Recurrent-Transformer",
+    description: "The paper proposes PathRWKV, a novel Recurrent-Transformer hybrid model for whole slide image (WSI) analysis in computational pathology. To address challenges in handling variable tile scales, model complexity, and training-inference trade-offs, PathRWKV integrates a dynamic recurrent structure for full-slide processing and adopts RWKV’s linear attention mechanism to reduce computational costs and mitigate overfitting. Multi-task learning jointly optimizes heterogeneous clinical indicators, improving training efficiency, while an asynchronous inference design enables sequential processing of all tiles during prediction. Evaluated across seven WSI datasets, PathRWKV achieves state-of-the-art performance in cancer subtyping, metastasis detection, and survival prediction, demonstrating superior generalization and scalability in pathology applications.",
+    date: "2025-03-05",
+    tags: "Image",
+    img: "images/papers-images/img-20250305-1.png",
+    link: "https://arxiv.org/abs/2503.03199"
+  },
+  {
     title: "Delta-WKV: A Novel Meta-in-Context Learner for MRI Super-Resolution",
     description: "The paper proposes Delta-WKV, a novel linear Transformer model for MRI super-resolution, integrating Meta-in-Context Learning (MiCL) and the Delta rule to dynamically adjust weights during inference for efficient local-global pattern recognition. Inspired by RWKV, Delta-WKV employs a quad-directional scanning mechanism and replaces traditional MLPs with a channel-mixing network, enhancing long-range dependency capture while preserving high-frequency details. Evaluated on IXI and fastMRI datasets, Delta-WKV achieves state-of-the-art PSNR/SSIM scores with 15% faster training and inference than SwinIR and MambaIR, demonstrating efficiency for clinical applications.",
     date: "2025-02-28",
